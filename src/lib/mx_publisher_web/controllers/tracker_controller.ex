c368e29f17db34514624dd0f7f9fc28348da047c
Max Grønlund <max@synthmax.dk>
2018-01-28 10:44:31 +0100

    tracker edit

--- src/lib/mx_publisher_web/controllers/tracker_controller.ex
+++ src/lib/mx_publisher_web/controllers/tracker_controller.ex
@@ -7,7 +7,7 @@ defmodule MxPublisherWeb.TrackerController do
   # plug :authenticate when action in [:index, :show]
 
   def index(conn, _params) do
-    
+
     trackers = MxNodes.list_trackers()
     render(conn, "index.html", trackers: trackers)
   end
@@ -16,7 +16,7 @@ defmodule MxPublisherWeb.TrackerController do
     changeset = MxNodes.change_tracker(%Tracker{})
     render(conn, "new.html", changeset: changeset)
   end
-  
+
   def create(conn, %{"tracker" => tracker_params}) do
     case MxNodes.create_tracker(tracker_params) do
       {:ok, tracker} ->
@@ -27,40 +27,40 @@ defmodule MxPublisherWeb.TrackerController do
         render(conn, "new.html", changeset: changeset)
     end
   end
-  
+
   def show(conn, %{"id" => id}) do
     tracker = MxNodes.get_tracker!(id)
     render(conn, "show.html", tracker: tracker)
   end
-# 
-  # def edit(conn, %{"id" => id}) do
-  #   user = Accounts.get_user!(id)
-  #   changeset = Accounts.change_user(user)
-  #   render(conn, "edit.html", user: user, changeset: changeset)
-  # end
-# 
-  # def update(conn, %{"id" => id, "user" => user_params}) do
-  #   user = Accounts.get_user!(id)
-# 
-  #   case Accounts.update_user(user, user_params) do
-  #     {:ok, user} ->
-  #       conn
-  #       |> put_flash(:info, "User updated successfully.")
-  #       |> redirect(to: user_path(conn, :show, user))
-  #     {:error, %Ecto.Changeset{} = changeset} ->
-  #       render(conn, "edit.html", user: user, changeset: changeset)
-  #   end
-  # end
-# 
+
+  def edit(conn, %{"id" => id}) do
+    tracker = MxNodes.get_tracker!(id)
+    changeset = MxNodes.change_tracker(tracker)
+    render(conn, "edit.html", tracker: tracker, changeset: changeset)
+  end
+
+  def update(conn, %{"id" => id, "tracker" => tracker_params}) do
+    tracker = MxNodes.get_tracker!(id)
+
+    case MxNodes.update_tracker(tracker, tracker_params) do
+      {:ok, tracker} ->
+        conn
+        |> put_flash(:info, "Tracker updated successfully.")
+        |> redirect(to: tracker_path(conn, :show, tracker))
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "edit.html", tracker: tracker, changeset: changeset)
+    end
+  end
+
   # def delete(conn, %{"id" => id}) do
   #   user = Accounts.get_user!(id)
   #   {:ok, _user} = Accounts.delete_user(user)
-# 
+#
   #   conn
   #   |> put_flash(:info, "User deleted successfully.")
   #   |> redirect(to: user_path(conn, :index))
   # end
-# 
+#
   # defp authenticate(conn, _opts) do
   #   if conn.assigns.current_user do
   #     conn
